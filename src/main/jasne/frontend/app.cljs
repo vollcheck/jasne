1461ca6d03e90e7ad58732383976e786983d626b
vollcheck <jaw@juxt.pro>
2022-07-06 00:56:06 +0200

    drawing o's

--- src/main/jasne/frontend/app.cljs
+++ src/main/jasne/frontend/app.cljs
@@ -1,7 +1,6 @@
 (ns jasne.frontend.app
   (:require [goog.dom :as dom]))
 
-
 (enable-console-print!)
 
 (def ctx (.getContext (dom/getElement "canvas") "2d"))
@@ -13,14 +12,44 @@
 
 (def game-state
   (atom [[" " " " " "]
-         [" " " " " "]
-         [" " " " " "]]))
+         ["x" " " " "]
+         ["o" " " " "]]))
 
 
 (defn tile-color [x y]
   (if (= (even? x) (even? y))
     "red"
     "black"))
+
+
+#_(defn draw-x! []
+  (.beginPath ctx)
+  (set! (.-lineWidth ctx) 5)
+  (set! (.-strokeStyle ctx) "white")
+  (set! (.moveTo ctx) 500 500)
+  (set! (.line ctx) 600 600)
+  (.stroke ctx)
+  )
+
+;; (draw-x!)
+
+(defn center [v r]
+  (+ v (/ r 2)))
+
+(defn draw-o!
+  ([x y] (draw-o! x y 20))
+  ([x y r]
+   (let [x (center x r)
+         y (center y r)]
+   (.beginPath ctx)
+   (.arc ctx x y r 0 (* Math/PI 2) false)
+   ;; (set! (.-fillStyle ctx) "blue")
+   ;; (.fill ctx)
+
+   (set! (.-lineWidth ctx) 3)
+   (set! (.-strokeStyle ctx) "black")
+   (.stroke ctx)
+   )))
 
 
 (defn draw-tile! [x y color]
@@ -55,15 +84,19 @@
   (set! (.-height (dom/getElement "canvas")) (inc (* h tile-size)))
   (set! (.-width (dom/getElement "canvas")) (inc (* w tile-size)))
 
-  (mapv
+  #_(mapv
     (fn [y]
       (mapv
         (fn [x] (draw-tile! (* tile-size x) (* tile-size y) (tile-color x y)))
         (range 0 w)))
-    (range 0 h)))
+    (range 0 h))
+  (draw-o! 0 0)
+
+  )
+
 
 (draw-board! 3 3)
-(mapv draw-unit! (:units @game-state))
+;; (mapv draw-unit! (:units @game-state))
 
 
 ;; (draw-tile! 0 0 "red")
